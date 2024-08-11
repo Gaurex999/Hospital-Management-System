ddba815a89502f09bc67b9c75cce35b5d8e09cce
Urwali Mahajan <urwali504@gmail.com>
2024-08-11 14:08:07 +0530

    Spring boot REST API

---
+++ Springboot Rest API/HospitalManagementSystemRest/src/test/java/com/example/demo/entities/DoctorEntity.java
@@ -0,0 +1,49 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "doctor")
+@Data
+
+public class DoctorEntity {
+
+	   @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    @Column(name = "doctor_id")
+	    private int doctorId;
+
+	    @Column(name = "user_id")
+	    private Integer userId;
+
+	    @Column(name = "first_name")
+	    private String firstName;
+
+	    @Column(name = "last_name")
+	    private String lastName;
+
+	    @Column(name = "address")
+	    private String address;
+
+	    @Column(name = "qualification")
+	    private String qualification;
+
+	    @Column(name = "contact_no")
+	    private Double contactNo;
+
+	    @Column(name = "email_id")
+	    private String emailId;
+
+	    @Column(name = "aadhar_no")
+	    private Double aadharNo;
+
+	    @Column(name = "department_id")
+	    private Integer departmentId;
+	
+}
---
+++ Springboot Rest API/HospitalManagementSystemRest/src/test/java/com/example/demo/services/DoctorService.java
@@ -0,0 +1,37 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.DoctorEntity;
+import com.example.demo.repositories.DoctorRepository;
+
+@Service
+public class DoctorService {
+	 @Autowired
+	    private DoctorRepository doctorRepository;
+
+	    public List<DoctorEntity> getAllDoctors() {
+	        return doctorRepository.findAll();
+	    }
+
+	    public Optional<DoctorEntity> getDoctorById(int doctorId) {
+	        Optional<DoctorEntity> doctor = doctorRepository.findById(doctorId);
+	        System.out.println("Fetching doctor with ID: " + doctorId);
+	        doctor.ifPresent(d -> System.out.println("Found doctor: " + d));
+	        return doctor;
+	    }
+
+	    public DoctorEntity saveDoctor(DoctorEntity doctor) {
+	        return doctorRepository.save(doctor);
+	    }
+
+	    public void deleteDoctor(int doctorId) {
+	        doctorRepository.deleteById(doctorId);
+	    }
+	    
+	}
+
